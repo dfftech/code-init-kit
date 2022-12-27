df749c84b3efa54973bb4f869ccb8e0b28e9c3ab
prasad <admin@dfftech.com>
2022-12-27 18:23:06 +0530

    reslove issues dynamic color

--- expo_kitten_api_ts-app/main/shared/UseColorScheme.web.ts
+++ expo_kitten_api_ts-app/main/shared/UseColorScheme.web.ts
@@ -1,5 +1,8 @@
 // useColorScheme from react-native does not support web currently. You can replace
 // this with react-native-appearance if you would like theme support on web.
+import {useColorScheme} from "react-native";
+
 export default function UseColorScheme() {
-  return "dark";
+  return useColorScheme();
+  //return "dark";
 }
